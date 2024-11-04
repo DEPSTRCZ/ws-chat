5f7e272475222e7d83e9788ba49b4f2e94d8c329
DEPSTRCZ <jiri.edelmann@gmail.com>
2024-11-04 13:08:28 +0100

    set path

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import http from "http";
 import { Server } from "socket.io";
+import path from "path";
 
 const app = express();
 const port = 3000;
@@ -9,6 +10,7 @@ const httpServer = http.createServer(app);
 const io = new Server(httpServer);
 
 app.set("view engine", "ejs");
+app.set("src/www", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
