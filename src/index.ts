9e56d613d5d4276ea16e96f15d3a1288cac04de4
DEPSTRCZ <jiri.edelmann@gmail.com>
2024-11-04 12:58:11 +0100

    socket server

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,25 @@
 import express from "express";
+import http from "http";
+import { Server } from "socket.io";
 
 const app = express();
 const port = 3000;
+
+const httpServer = http.createServer(app);
+const io = new Server(httpServer);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`Server started at http://localhost:${port}`);
+
+io.on("connection", (socket) => {
+    console.log("A user connected");
+    socket.on("disconnect", () => {
+        console.log("User disconnected");
+    });
+});
+
+httpServer.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
 });
