ff02f9ccd0009da15d44055192f1277e94669d3d
DEPSTRCZ <jiri.edelmann@gmail.com>
2024-11-05 09:02:19 +0100

    alpha security

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import http from "http";
-import { Server } from "socket.io";
+import { Server, Socket } from "socket.io";
 import path from "path";
 import bodyParser from "body-parser";
 import jwt from "jsonwebtoken";
@@ -14,13 +14,27 @@ const port = 3000;
 const JWT_SECRET = "secretASKOHDIZIOASDGZIGUI7654465654654asdasd__-asdasd%";
 
 const httpServer = http.createServer(app);
-const io = new Server(httpServer);
+const io = new Server(httpServer, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+    },
+});
 
 let messages = new Array();
 let users = new Array();
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
+
+//specify cors to allow all
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Headers", "*");
+    next();
+});
 
 
 app.set("view engine", "ejs");
@@ -29,16 +43,25 @@ app.set("views", path.join(__dirname, "www"));
 app.get("/chat", (req, res) => {
     const params = req.query;
 
-    const token = params.token;
+    const token = params.token as string;
 
-    if (!token || !users.find((user) => user.token === token)) {
-        res.status(401).send("Bad Request");
+    if (!token) {
+        res.status(400).send("Bad Request");
         return;
     }
 
-    const user = users.find((user) => user.token === token);
+    // Verify the token
+    try {
+        jwt.verify(token, JWT_SECRET);
+    } catch (e) {
+        res.status(401).send("Unauthorized");
+        return;
+    }
 
-    res.render('chat', {userName: user.name,messages: messages});
+
+    //const user = users.find((user) => user.token === token);
+
+    res.render('chat');
 });
 
 app.get("/", (req, res) => {
@@ -62,7 +85,7 @@ app.post("/init", async (req, res) => {
     const user = {
         id: userId,
         name: name,
-        token: token
+        uuid: userId,
     };
 
     users.push(user);
@@ -71,19 +94,44 @@ app.post("/init", async (req, res) => {
 });
 
 
-io.on("connection", (socket) => {
-    console.log("A user connected");
-    socket.on("disconnect", () => {
-        console.log("User disconnected");
-    });
+interface AuthenticatedSocket extends Socket {
+    user?: any;
+}
+
+io.use(async (socket: AuthenticatedSocket, next) => {
+    const token = socket.handshake.query.token as string; // JWT passed in query during connection
+
+    if (!token) {
+        return next(new Error("Authentication error: Token missing"));
+    }
+
+    // Verify the token and attach user info to the socket
+    try {
+        const user = jwt.verify(token, JWT_SECRET);
+
+        if (!user) {
+            return next(new Error("Authentication error: Invalid token"));
+        }
+        socket["user"] = user;
+
+        next();
+    } catch (e) {
+        return next(new Error("Authentication error: "+e));
+    }
+
+
 });
 
-io.on('connection', (socket) => {
+
+io.on('connection', (socket:AuthenticatedSocket) => {
+
+    const user = socket.user;
     socket.on('message', (data) => {
       console.log('Received data:', data);
       // You can emit a response back to the client here
+
+      data.userName += user.name;
       messages.push(data);
-      console.log(messages)
         io.emit('serverMessage', data);
     });
     socket.on('getPast', () => {
--- src/www/functions.js
+++
@@ -1,6 +0,0 @@
-function formatTime(time) {
-    if (String(time).length == 1) {
-        return 0 + time;
-    } 
-    return String(time);
-}
